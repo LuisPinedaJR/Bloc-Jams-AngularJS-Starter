25d970d6089d9fd2cd6476e7d9130a41226222c8
Luis Pineda JR <lspnd00@gmail.com>
2017-12-02 14:04:08 -0500

    playsong function

---
+++ app/scripts/services/SongPlayer.js
@@ -0,0 +1,65 @@
+(function(){
+      function SongPlayer(){
+              var SongPlayer = {};
+
+              var currentSong = null;
+
+              /**
+             * @desc Buzz object audio file
+             * @type {Object}
+             */
+              var currentBuzzObject = null;
+
+              /**
+             * @function playSong
+             * @desc currently playing song 
+             * @param {Object} song
+             */
+
+             var playSong =  function (song){
+                  currentBuzzObject.play();
+                  song.playing = true;
+
+             }
+
+              /**
+             * @function setSong
+             * @desc Stops currently playing song and loads new audio file as currentBuzzObject
+             * @param {Object} song
+             */
+
+              var setSong =  function(song){
+                  if (currentBuzzObject){
+                      currentBuzzObject.stop();
+                      currentSong.playing = null;
+                  }
+                  currentBuzzObject = new buzz.sound(song.audioUrl, {
+                          formats: ['mp3'],
+                          preload: true
+                  });
+};
+
+              SongPlayer.play = function(song){
+                    if (currentSong !== song){
+
+                  setSong(song);
+                  currentBuzzObject.play();
+                  song.playing = true;
+
+            } else if (currentSong === song){
+                if (currentBuzzObject.isPaused()){
+                      currentBuzzObject.play();
+                }
+          }
+        };
+              SongPlayer.pause = function(song){
+                      currentBuzzObject.pause();
+                      song.playing = false;
+                };
+              return SongPlayer;
+      }
+
+      angular
+            .module('blocJams')
+            .factory('SongPlayer', SongPlayer);
+})();
